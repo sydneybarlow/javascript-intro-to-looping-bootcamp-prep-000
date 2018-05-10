9e0bf4acf63a59d3f63d590443953f0d1b4d1d61
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-10 20:30:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,10 +17,13 @@ function whileLoop(t) {
 }
 
 function doWhileLoop(array) {
+
+    function maybeTrue() {
   return Math.random() >= 0.5
 }
-  do {
-  console.log('doo-bee-doo-bee-doo')
-} while (array.length++ > 0 && maybeTrue());
-  return array
-}
+ 
+    do {
+      array.pop()
+    } while (maybeTrue());
+    return array
+  }
